57e4f8d5a61bbc079f9ee8779c35715b935f5128
onomojiji <onomojj@gmail.com>
2020-12-31 00:00:43 +0100

    Ajout d'un compteur d'instruction pour chaque tri...

--- fonctionSort.py
+++ fonctionSort.py
@@ -1,6 +1,6 @@
 
 ''' 
-    Titre : programme complet de tri Basique des elemments d'un tableau
+    Titre : programme complet de tri Basique des elemments d'un tableau ;
             programme de la fonction python sort()
 
     Evolution du code :
@@ -23,28 +23,34 @@
 
 ''' tableau = [7,123,78,1,4,0,454,9] '''
 
-tableau = [7,9,0,454,4,78,1,123,-3,-13,79,23,34,645545454,0.34]
+tableau = [7, 17, 9,0,454,4,78,1,123,-3,-13,79,28,34,654,23,0.34,25]
 
 nbElements = len(tableau)
 
+N = 0
+N2 = 0
+
 def OrdreCroissant(tab:list):
+    
+    global N
 
     # l'objectif ici étant de classer par ordre croissant les éléments du tableau :
-    
+ 
     def remplaceCroissant():
 
+        global N
 
         for k in range(0, i) : #pour chaque nombre allant de la position 0 à la position i :
             
             if tableau[k] > tableau[i] :  #Si le nombre correspondant à la position k > au nombre à la position i
-                Vk = tableau[i]           #
-                tableau[i] = tableau[k]   # On permutte leurs positions
-                tableau[k] = Vk           #
+                Vk = tableau[i] ; N += 1
+                tableau[i] = tableau[k] ; N += 1   # On permutte leurs positions
+                tableau[k] = Vk ; N += 1        #
 
             elif tableau[k] > tableau[k+1]: #En même temps si le nombre correspondant à la position k  au nombre à la position k+1
-                Vk = tableau[k+1]           #
-                tableau[k+1] = tableau[k]   # On pemutte leurs positions
-                tableau[k] = Vk             #
+                Vk = tableau[k+1] ; N += 1           #
+                tableau[k+1] = tableau[k] ; N += 1   # On pemutte leurs positions
+                tableau[k] = Vk  ; N += 1            #
                 
             else :
                 continue
@@ -52,34 +58,39 @@ def OrdreCroissant(tab:list):
     for i in range(0, nbElements) : 
 
         if tableau[0] > tableau[i] : # Je compare le nombre à la position i au nombre à la position 0 d'abord pour me rassurer que mon derrière est déjè bon.
-            Vo = tableau[0]
-            tableau[0] = tableau[i]  # La même chanson
-            tableau[i] = Vo
+            Vo = tableau[0] ; N += 1
+            tableau[0] = tableau[i] ; N += 1  # La même chanson
+            tableau[i] = Vo ; N += 1
             remplaceCroissant()
                 
         else:
             remplaceCroissant()
 
-    print(f"\n TabOrdCroissant = {tab}\n")
+    print(f"\n TabOrdCroissant = {tab}\n\
+                Nombre d'instructions = {N}")
 
 
 def OrdreDecroissant(tab:list):
+
+    global N2
 
     # l'objectif ici étant de classer par ordre décroissant les éléments du tableau :
 
     def remplaceDecroissant():
 
+        global N2
+
         for k in range(0, i) :
             
             if tableau[i] > tableau[k] :#Si le nombre correspondant à la position k > au nombre à la position i
-                Vk = tableau[i]          #
-                tableau[i] = tableau[k]  #On permutte leurs valeurs
-                tableau[k] = Vk          #
+                Vk = tableau[i]   ; N2 += 1        #
+                tableau[i] = tableau[k]  ; N2 += 1 #On permutte leurs valeurs
+                tableau[k] = Vk     ; N2 += 1      #
 
-            elif tableau[k+1] > tableau[k] : # En même temps si le nombre correspondant à la position k  au nombre à la position k+1
-                Vk = tableau[k+1]
-                tableau[k+1] = tableau[k]    # On pemutte leurs positions
-                tableau[k] = Vk
+            elif tableau[k+1] > tableau[k] :   # En même temps si le nombre correspondant à la position k  au nombre à la position k+1
+                Vk = tableau[k+1]  ; N2 += 1
+                tableau[k+1] = tableau[k]  ; N2 += 1   # On pemutte leurs positions
+                tableau[k] = Vk  ; N2 += 1
                 
             else :
                 continue
@@ -87,15 +98,16 @@ def OrdreDecroissant(tab:list):
     for i in range(nbElements-1, 0,-1) :
     
         if tableau[i] > tableau[0] : # La même chanson...
-            Vo = tableau[0]
-            tableau[0] = tableau[i]
-            tableau[i] = Vo
-            remplaceDecroissant()
+            Vo = tableau[0]  ; N2 += 1
+            tableau[0] = tableau[i]  ; N2 += 1
+            tableau[i] = Vo  ; N2 += 1
+            remplaceDecroissant()  ; N2 += 1
                 
         else:
             remplaceDecroissant()
 
-    print(f"\n TabOrdeCroissant = {tab}\n")
+    print(f"\n TabOrdDecroissant = {tab}\n\
+                Nombre d'instructions = {N2}\n")
 
 
 OrdreCroissant(tableau)
